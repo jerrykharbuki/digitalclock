871b0a95a0e4b01e2ed39f7b0b4af65e40f0492c
Jerry Kharbuki <jerrykharbuki83@gmail.com>
2025-08-31 21:14:17 +0530

    first commit

---
+++ ifst.js
@@ -0,0 +1,9 @@
+let age ;
+document.getElementById("mysub").onclick=function
+ age=document.getElementById("label").value ;
+  if(age>=18){
+    console.log("eligible to vote ");
+  }
+  else{
+    console.log("not eligible")
+  }
